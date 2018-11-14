020f203b273cedbf75f0f558c4b5b63fc134e98e
marlianurhuda <43769705+marlianurhuda@users.noreply.github.com>
2018-11-14 16:33:05 +0700

    Add files via upload

---
+++ herbal/application/views/admin/header.php
@@ -0,0 +1,8 @@
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Herbal's Store - Edit Kategori Barang</title>
+	<link href="<?php echo base_url('assets1/css/bootstrap.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets1/css/font-awesome.min.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets1/css/datepicker3.css'); ?>" rel="stylesheet">
+	<link href="<?php echo base_url('assets1/css/styles.css'); ?>" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
